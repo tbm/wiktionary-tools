d8edaac0d2aab38fb130ece0a2db9bf769ca9554
Martin Michlmayr <tbm@cyrius.com>
2024-01-16 20:24:28 +0800

    Avoid overriding the whole page
    Since we're getting the entry only, we have to replace the entry
    with the new one rather replacing the whole page (which would
    remove other-language entries).

--- kamusi/edit.py
+++ kamusi/edit.py
@@ -2,21 +2,15 @@
 Edit Wiktionary pages
 """
 
-from .entry import get_entry
-
-
-def add_wikipedia(text, lang):
+def add_wikipedia(entry, lang):
     """
-    Add Wikipedia link to text
+    Add Wikipedia link to  entry
     """
-    text = get_entry(text, lang)
-    if not text:
-        return None
-    if "{{wikipedia" in text or "{{wp" in text:
-        return text
-    first_break = text.find("\n\n") + 1
+    if "{{wikipedia" in entry or "{{wp" in entry:
+        return entry
+    first_break = entry.find("\n\n") + 1
     wp = "{{wikipedia"
     if lang != "en":
         wp += "|lang=" + lang
     wp += "}}"
-    return text[:first_break] + wp + "\n" + text[first_break:]
+    return entry[:first_break] + wp + "\n" + entry[first_break:]
