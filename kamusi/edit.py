0ff33912cad441122a962c575b8e1f68b717e886
Martin Michlmayr <tbm@cyrius.com>
2024-01-16 20:05:37 +0800

    Don't add "lang" parameter for English Wikipedia link

--- kamusi/edit.py
+++ kamusi/edit.py
@@ -15,5 +15,8 @@ def add_wikipedia(text, lang):
     if "{{wikipedia" in text or "{{wp" in text:
         return text
     first_break = text.find("\n\n") + 1
-    wp = "{{wikipedia|lang=" + lang + "}}"
+    wp = "{{wikipedia"
+    if lang != "en":
+        wp += "|lang=" + lang
+    wp += "}}"
     return text[:first_break] + wp + "\n" + text[first_break:]
