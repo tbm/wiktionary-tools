7581c08efe84cb4269dad085acc9fde3193edb89
Martin Michlmayr <tbm@cyrius.com>
2024-01-16 20:35:54 +0800

    Add script to add category to entry

--- kamusi/edit.py
+++ kamusi/edit.py
@@ -2,6 +2,26 @@
 Edit Wiktionary pages
 """
 
+import re
+
+
+def add_category(entry, category, lang):
+    """
+    Add category to entry
+    """
+
+    if re.search(r"({{(C|c|topics)\||\[\[Category:)", entry):
+        print("Page already has category")
+        return entry
+
+    cat = "\n{{C|" + lang + "|" + category + "}}\n"
+    if entry[-1] != "\n":
+        entry += "\n" + cat
+    else:
+        entry += cat
+    return entry
+
+
 def add_wikipedia(entry, lang):
     """
     Add Wikipedia link to  entry
