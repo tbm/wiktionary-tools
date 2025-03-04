d33a6445efe2f2134fc75bb5e47c8aca64510a8e
Martin Michlmayr <tbm@cyrius.com>
2025-03-04 18:37:25 +0800

    Match categories only at the beginning of the line
    Some entries have "# {{C...}}" and we don't want to match that.

--- kamusi/edit.py
+++ kamusi/edit.py
@@ -52,7 +52,7 @@ def add_ref(entry, ref):
     if loc == -1:
         loc = entry.find("===Anagrams===")
     if loc == -1:
-        if match := re.search(r"(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
+        if match := re.search(r"^(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
             loc = match.start()
     if loc == -1:
         entry = entry.rstrip("\n") + "\n\n"
