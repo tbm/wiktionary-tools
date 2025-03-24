tbm/wiktionary-tools

commit d33a6445efe2f2134fc75bb5e47c8aca64510a8e

Match categories only at the beginning of the line
Some entries have "# {{C...}}" and we don't want to match that.

kamusi/edit.py
@@ -52,7 +52,7 @@ def add_ref(entry, ref):
     if loc == -1:
         loc = entry.find("===Anagrams===")
     if loc == -1:
-        if match := re.search(r"(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
+        if match := re.search(r"^(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
             loc = match.start()
     if loc == -1:
         entry = entry.rstrip("\n") + "\n\n"

commit 1f88713dc4104965b607ba7123fec90feb70d01c

Fix detection of categories
Commit commit d33a6445ef ("Match categories only at the beginning of the line")
broke the detection of categories.

kamusi/edit.py
@@ -52,7 +52,7 @@ def add_ref(entry, ref):
     if loc == -1:
         loc = entry.find("===Anagrams===")
     if loc == -1:
-        if match := re.search(r"^(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
+        if match := re.match(r"(\{\{\s*(topics|c|C|cln)\s*\||\[\[Category:\w+:)", entry):
             loc = match.start()
     if loc == -1:
         entry = entry.rstrip("\n") + "\n\n"