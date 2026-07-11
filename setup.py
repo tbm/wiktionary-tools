2a6e173bbbe05c37065d3c93d524d830a96002ac
Kiril Kovachev <ktkovachev@proton.me>
2026-07-11 19:14:22 +0100

    build: add setup.cfg/setup.py to enable installing kamusi as module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kamusi",
+    description="Library for comparing cross-language Wiktionary data",
+    url="https://github.com/tbm/wiktionary-tools",
+    author="Martin Michlmayr",
+    author_email="tbm@cyrus.org",
+    license="GPL 3",
+    packages=["kamusi"]
+)
